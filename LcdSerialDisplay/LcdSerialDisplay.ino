1a7fa1cf72e193051c2159c2dcd4141457561f69
Patrick Caldwell <patrick@mobilemagicdevelopers.com>
2014-02-13 01:41:59 -0500

    displaying git info
    -- implemented repo monitor
    -- added delta custom character
    -- added delay for arduino startup

--- LcdSerialDisplay/LcdSerialDisplay.ino
+++ LcdSerialDisplay/LcdSerialDisplay.ino
@@ -4,7 +4,8 @@
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
 uint8_t duck[8]  = {0x0, 0xc, 0x1d, 0xf, 0xf, 0x6, 0x0};
-uint8_t check[8] = {0x0, 0x1 ,0x3, 0x16, 0x1c, 0x8, 0x0};
+uint8_t check[8] = {0x0, 0x1, 0x3, 0x16, 0x1c, 0x8, 0x0};
+uint8_t delta[8] = {0x0, 0x4, 0x4, 0xa, 0xa, 0x11, 0x1f};
 
 void setup()  
 {
@@ -12,14 +13,15 @@ void setup()
   lcd.begin(20, 4);   
   lcd.createChar(1, duck);
   lcd.createChar(2, check);
+  lcd.createChar(3, delta);
   lcd.clear();
-  
-  lcd.noBacklight();
-  delay(406);
   lcd.backlight();
+
+  delay(2000);
 }
 
 int currentLine = 0;
+int currentColumn = 0;
 
 void loop()
 {
@@ -30,18 +32,28 @@ void loop()
     if (currentLine > 3) {
       lcd.clear();
       currentLine = 0;
+      currentColumn = 0;
     }
    
     while (Serial.available() > 0) {
       char character = Serial.read();    
+
       if (character == '\n') {
-        currentLine += 1;
-        
-        lcd.setCursor(0, currentLine); 
+        currentLine++;
+        currentColumn = 0;
+        lcd.setCursor(currentColumn, currentLine); 
       } 
-      
-      else
+      else {
+        if (currentColumn > 19) {
+          currentLine++;
+          currentColumn = 0;
+        }
+
+        lcd.setCursor(currentColumn, currentLine); 
+        
         lcd.write(character);
+        currentColumn++;
+      }
     }
   }
 }
---
+++ git_repo_mon.rb
@@ -0,0 +1,47 @@
+require 'serialport'
+
+def get_branch
+  `git rev-parse --abbrev-ref HEAD 2>/dev/null`.strip
+end
+
+def get_unpushed_commits
+  `git rev-list @{u}.. --count 2>/dev/null`.to_i
+end
+
+def get_unmerged_commits
+  `git rev-list ..FETCH_HEAD --count 2>/dev/null`.to_i
+end
+
+def get_changed_file_count
+  `git status --p 2>/dev/null | wc -l | tr -d ' '`.to_i
+end
+
+def get_user_name
+  `git config user.name`.strip
+end
+
+def get_last_commit(user_name)
+  `git log --author="#{user_name}" --pretty=format:%cr -1 2>/dev/null` || 'never'
+end
+
+SerialPort.open(ARGV[0], 9600, 8, 1, SerialPort::NONE) do |port|
+  sleep 2
+
+  while true
+    status = "+#{get_unpushed_commits}-#{get_unmerged_commits}"
+    branch_length = 19 - status.length
+    branch = get_branch[0, branch_length].ljust(branch_length)
+    message = "#{branch} #{status}"
+    port.print "#{message}\n"
+
+    user_name = get_user_name
+
+    last_commit = get_last_commit user_name
+    change_count = get_changed_file_count.to_s.ljust(18 - last_commit.length)
+    port.write 0x3.chr
+    port.print "#{change_count} #{last_commit}\n\n"
+
+    port.print "#{user_name[0..20].center(20)}\n"
+    sleep 10
+  end
+end
